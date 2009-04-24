9be6c58dbee33b6f901fdd6311fdaaafbcd9a43e
Travis B. Hartwell <nafai@travishartwell.net>
2009-04-24 02:29:04 -0600

    Added function to open a project from a list of projects. Abstracted out the ido project functionality that is also used in .emacs with magit-status-repos. metaproject file for this project.

--- metaproject.el
+++ metaproject.el
@@ -1,6 +1,12 @@
+(require 'project-utils)
+
 (defconst metaproject-config-file-name ".metaproject"
   "This is the default filename used for the metaproject configuration
 files for each project")
+
+; Probably should be a customization
+(defvar metaproject-project-dirs nil
+  "This is a list of the directories that contain project directories")
 
 (defun metaproject-read-from-file (file)
   (with-temp-buffer
@@ -10,7 +16,7 @@ files for each project")
 
 (defun metaproject-open-project-files (project-config)
   (let ((files (plist-get project-config 'files))
-	(project-base-dir (plist-get project-config 'project-base-dir)))
+        (project-base-dir (plist-get project-config 'project-base-dir)))
     (mapc
      (lambda (f)
        (find-file (expand-file-name f project-base-dir)))
@@ -18,11 +24,24 @@ files for each project")
 
 (defun metaproject-open-project (project-dir)
   (let ((project-file-name
-	 (expand-file-name metaproject-config-file-name project-dir)))
+         (expand-file-name metaproject-config-file-name project-dir)))
     (if (file-exists-p project-file-name)
-	(let ((project-config (metaproject-read-from-file project-file-name)))
+        (let ((project-config (metaproject-read-from-file project-file-name)))
 	  (metaproject-open-project-files project-config))
       (message "No project found in directory %s" project-dir))))
 
-(provide 'metaproject)
 
+(defun metaproject-is-metaproject (dir)
+  (file-exists-p (expand-file-name metaproject-config-file-name dir)))
+
+(defun metaproject-get-top-dir (dir)
+  nil)
+
+(defun metaproject-find-metaprojects (dir)
+  (project-util-find #'metaproject-is-metaproject dir))
+
+(defun metaproject-open-ido (arg)
+  (interactive "P")
+  (project-util-action-ido #'metaproject-find-metaprojects metaproject-project-dirs #'nil #'metaproject-open-project "Metaproject Project to open? " arg))
+
+(provide 'metaproject)
---
+++ project-utils.el
@@ -0,0 +1,38 @@
+(require 'esh-util)
+
+(defun project-util-find (desired-dir-p dir)
+  (when (file-directory-p dir)
+    (or (char-equal ?/ (aref dir (1- (length dir))))
+	(setq dir (file-name-as-directory dir)))
+    (if (funcall desired-dir-p dir) dir
+      (let ((files (directory-files dir t nil t))
+	    fullname file)
+	(eshell-flatten-list (remove-if
+			      'null
+			      (mapcar
+			       (lambda (file)
+				 (unless
+				     (or
+				      (string= (substring file -3) "/..")
+				      (string= (substring file -2) "/."))
+				   (project-util-find desired-dir-p file)))
+			       files)))))))
+
+
+(defun project-util-action-ido (find-func top-level-dirs get-top-dir action-function prompt arg)
+  (interactive "P")
+  (let* ((project-dirs (eshell-flatten-list (mapcar find-func top-level-dirs)))
+	 (buffer-dir (when (not (null buffer-file-name))
+		       (expand-file-name (if (file-directory-p buffer-file-name)
+					     buffer-file-name
+					   (file-name-directory buffer-file-name)))))
+	 (this-project-dir (if (and
+				(fboundp get-top-dir)
+				(not (null buffer-dir))
+				(null arg))
+			       (file-name-as-directory (funcall get-top-dir buffer-dir))
+			     nil)))
+    (funcall action-function (ido-completing-read prompt project-dirs nil nil nil))))
+
+(provide 'project-utils)
+
